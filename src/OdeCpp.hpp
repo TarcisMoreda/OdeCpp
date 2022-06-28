8d293b3703c54366a78ba6cd0975f09fae3d02cc
TarcisMoreda <tarcismoreda@gmail.com>
2022-06-27 21:07:51 -0300

    aaa

--- src/OdeCpp.hpp
+++ src/OdeCpp.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "./Functions/AlphaFunction.hpp"
-#include "AlphaFunction.hpp"
-#include "IzhikevichModel.hpp"
+#include "./Neurons/OdeModel.hpp"
+#include "./Neurons/AlphaFunction.hpp"
+#include "./Neurons/IzhikevichModel.hpp"
 #include "Observer.hpp"
 #include "OdeSolver.hpp"
 #include "Ode1Solver.hpp"
